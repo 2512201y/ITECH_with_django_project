9d43fd0349dc474a533d68664d6c510c426f1e77
2512201y <2512201y@student.gla.ac.uk>
2021-07-30 07:57:22 +0800

    Test_Chapter6

--- rango/models.py
+++ rango/models.py
@@ -1,16 +1,20 @@
-from os import name
-from rango import views
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128,unique=True)
+    name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
-
 
     def __str__(self):
         return self.name
@@ -21,7 +25,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.title
--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.urls import path
-from django.urls.conf import include
 from rango import views
 
 app_name = 'rango'
@@ -8,4 +6,5 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/',views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
